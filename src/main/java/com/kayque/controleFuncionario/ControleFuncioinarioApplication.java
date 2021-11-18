e3c7ea4a204addd7ed5dc9de5013bb59c164c3df
kayquec13 <kayquecbenatti@gmail.com>
2021-11-17 22:40:00 -0300

    Projeto Criado

---
+++ src/main/java/com/kayque/controleFuncionario/ControleFuncioinarioApplication.java
@@ -0,0 +1,13 @@
+package com.kayque.controleFuncionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFuncioinarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFuncioinarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kayque/controleFuncionario/ControleFuncioinarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kayque.controleFuncionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFuncioinarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
