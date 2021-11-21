1e4696905e8c9e2eb31bd8a069fcda9f0731aa48
kayquec13 <kayquecbenatti@gmail.com>
2021-11-21 19:26:29 -0300

    Instalando dependencias do banco h2

--- src/main/java/com/kayque/controleFuncionario/entities/Cargo.java
+++ src/main/java/com/kayque/controleFuncionario/entities/Cargo.java
@@ -3,10 +3,18 @@ package com.kayque.controleFuncionario.entities;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cargo implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idCargo;
 	private String nomeCargo;
 	private BigDecimal salario;
--- src/main/java/com/kayque/controleFuncionario/entities/Funcionario.java
+++ src/main/java/com/kayque/controleFuncionario/entities/Funcionario.java
@@ -1,6 +1,8 @@
 package com.kayque.controleFuncionario.entities;
 
-public class Funcionario {
+import java.io.Serializable;
+
+public class Funcionario implements Serializable {
 	private Integer id;
 	private String nome;
 	private Cargo cargo;
@@ -9,4 +11,66 @@ public class Funcionario {
 	public Funcionario() {
 		
 	}
+
+	public Funcionario(Integer id, String nome, Cargo cargo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cargo = cargo;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public Cargo getCargo() {
+		return cargo;
+	}
+
+
+	public void setCargo(Cargo cargo) {
+		this.cargo = cargo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Funcionario other = (Funcionario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 }
